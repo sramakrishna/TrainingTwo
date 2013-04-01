9c8e773e473b21d7fb135a7aefce22fa68b6be81
Ramakrishna Sharvirala <ramakrishna.sharvirala@gmail.com>
2013-04-01 16:14:02 +0530

    #DEMO-42 Committing latest changes

--- src/main/java/com/wipro/atlassian/TrainingPartTwo.java
+++ src/main/java/com/wipro/atlassian/TrainingPartTwo.java
@@ -16,5 +16,7 @@ public class TrainingPartTwo {
 		System.out.println("This is test line");
 		
 		System.out.println("Added Third Statement");
+		
+		System.out.println("This is 4th Statement");
 	}
 }
