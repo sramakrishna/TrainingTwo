d3b33a94af417725f85938778f7e539d5a20b12f
Ramakrishna Sharvirala <ramakrishna.sharvirala@gmail.com>
2013-04-01 12:33:48 +0530

    Made Changes

--- src/main/java/com/wipro/atlassian/TrainingPartTwo.java
+++ src/main/java/com/wipro/atlassian/TrainingPartTwo.java
@@ -12,5 +12,7 @@ public class TrainingPartTwo {
 	
 	public static void main(String[] args) {
 		System.out.println("This is Hello World Program");
+		
+		System.out.println("This is test line");
 	}
 }
