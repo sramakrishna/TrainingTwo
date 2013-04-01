922e50e96bde7aa43261551d3e4c252eb5959d32
Ramakrishna Sharvirala <ramakrishna.sharvirala@gmail.com>
2013-04-01 11:18:12 +0530

    Simple Maven Project

---
+++ src/main/java/com/wipro/atlassian/TrainingPartTwo.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.wipro.atlassian;
+
+/**
+ * @author sharvir
+ *
+ */
+public class TrainingPartTwo {
+
+	
+	public static void main(String[] args) {
+		System.out.println("This is Hello World Program");
+	}
+}
---
+++ src/test/java/com/wipro/atlassian/AppTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.wipro.atlassian;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Ramakrishna Sharvirala
+ *
+ */
+public class AppTest extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@After
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
